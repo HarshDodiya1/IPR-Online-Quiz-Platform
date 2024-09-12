30971e1448ba32804a7323ae922dc7d48e50b777
slantie <kandarp.gajjar@gmail.com>
2024-09-12 11:33:39 +0530

    Admin Setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,36 @@
 import { useTranslation } from "react-i18next";
 import "./App.css";
 import LanguageSelector from "./components/LanguageSelector";
+import React from "react";
+import AdminLanding from "./pages/Adminlanding";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AdminUploadExcel from "./pages/AdminUploadExcel";
 
 function App() {
   const { t } = useTranslation();
   const { part1, part2 } = t("description");
   return (
-    <div className="flex flex-col items-center justify-center h-screen gap-10">
-      <div className="text-xl">
-        <LanguageSelector />
-      </div>
-      <div className="font-bold text-4xl">{t("greetings")}</div>
-      <div className="text-lg text-center">
-        {part1}
-        <br />
-        {part2}
-      </div>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/admin" element={<AdminLanding />} />
+          <Route path="/admin/upload-excel" element={<AdminUploadExcel />} />
+          <></>
+        </Routes>
+      </Router>    
+      {/* <div className="flex flex-col items-center justify-center h-screen gap-10">
+        
+        <div className="text-xl">
+          <LanguageSelector />
+        </div>
+        <div className="font-bold text-4xl">{t("greetings")}</div>
+        <div className="text-lg text-center">
+          {part1}
+          <br />
+          {part2}
+        </div>
+      </div> */}
+    </>
   );
 }
 
---
+++ frontend/src/pages/AdminLanding.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const AdminLanding = () => {
+  return (
+    <>
+        Adminlanding
+    </>
+  )
+}
+
+export default AdminLanding;
