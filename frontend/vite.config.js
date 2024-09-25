af25d9190fd38f6f22ed1cf150727184f7cfffba
HarshDodiya1 <dodiyaharsh999@gmail.com>
2024-09-25 21:52:07 +0530

    Refactor Vercel deployment configurations and fix import statements
    - Removed vercel.json file as it was no longer needed for deployment
    - Updated import statements in vite.config.js to resolve path issues on deployment
    
    Closes: #123

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from 'path';
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {
-    host: '0.0.0.0', 
+    host: "0.0.0.0",
     proxy: {
       "/api": "https://backend-ipr.vercel.app",
-      secure: false,
+      changeOrigin: true,
+      secure: true,
     },
   },
   plugins: [react()],
